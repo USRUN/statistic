33c04aafb8255126593fb383f181964d485675fa
phuctt4 <phuctt4@vng.com.vn>
2020-08-23 19:29:55 +0700

    update prefix key of redis

--- src/main/java/usrun/utility/CacheKeyGenerator.java
+++ src/main/java/usrun/utility/CacheKeyGenerator.java
@@ -1,5 +1,6 @@
 package usrun.utility;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,51 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class CacheKeyGenerator {
 
-  public String keyVerifyOtp(long userId) {
-    return "users:otp:" + userId;
+  private final String PREFIX;
+
+  public CacheKeyGenerator(@Value("${spring.profiles.active}") String env) {
+    this.PREFIX = env + ":";
   }
 
-  public String keyTrack(long trackId) {
-    return "track:" + trackId;
-  }
-
-  public String keyUser(long userId) {
-    return "user:" + userId;
-  }
-
-  public String keyUserEmail(String email) {
-    return "users:email:" + email;
-  }
-
-  public String keyTrackSig(long trackId, String sig) {
-    return "track:sig:" + trackId + sig;
-  }
-
-  public String keyTeamMemberType(long teamId, long userId) {
-    return "team:role:" + teamId + ":" + userId;
-  }
-
-  public String keyActivity(long activityId) {
-    return "activity:" + activityId;
-  }
-
-  public String keyActivitySortedSet(long teamId) {
-    return "activities:team:" + teamId;
-  }
-
-  public String keyActivityCountByTeam(long teamId) {
-    return "activities:team:count:" + teamId;
-  }
-
-  public String keyActivityLock(long userId, long time) {
-    return "activity:lock:" + userId + ":" + time;
-  }
-  
-  public String keyTeamStat(){
-      return"team:stat:";
-  }
-
-  public String keyLoveCount(long activityId) {
-    return "activity:love:" + activityId;
+  public String keyTeamStat() {
+    return PREFIX + "team:stat:";
   }
 }
